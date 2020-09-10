8bb3999d50805ef9410f60a12a80c5f012c4c6e3
Justin Linn <jelinn@gmail.com>
2020-09-09 20:43:59 -0500

    updates

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "null_resource" "configure-hashicups" {
     inline = [
       "sudo add-apt-repository universe",
       "sudo apt -y update",
-      "sudo apt install docker.io",
+      "sudo apt -y install docker.io",
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
