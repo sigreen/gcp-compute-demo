2e007e72c7ed264c644abe01b97c3a032b223a3f
Simon Green <13749692+sigreen@users.noreply.github.com>
2021-03-23 12:48:47 -0400

    Added output

--- output.tf
+++ output.tf
@@ -12,3 +12,11 @@ output "priv-key"{
   value = tls_private_key.ssh-key.private_key_pem
   sensitive = true
 }
+
+output "network"{
+  sensitive = true
+  }
+
+output "subnets"{
+  sensitive = true
+  }
