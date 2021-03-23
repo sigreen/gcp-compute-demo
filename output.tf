028cc184311cf515a18ffda52f98c302ca1ff35a
Simon Green <13749692+sigreen@users.noreply.github.com>
2021-03-23 12:47:15 -0400

    Fix sensitive

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "hashicups_url" {
   value = "http://${google_compute_instance.hashicups.network_interface.0.access_config.0.nat_ip}"
+  sensitive = true
 }
 
 output "private_ip" {
   value = google_compute_instance.hashicups.network_interface.0.network_ip
+  sensitive = true
 }
 
 output "priv-key"{
   value = tls_private_key.ssh-key.private_key_pem
+  sensitive = true
 }
